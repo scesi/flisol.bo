a3f0c3c01ca03d3b61ebb4cf3632da05f3185c6e
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2025-04-20 13:05:05 -0400

    Fix: accesibility onkey down-up

--- src/components/ui/arrow-toggle/arrow-toggle.tsx
+++ src/components/ui/arrow-toggle/arrow-toggle.tsx
@@ -12,9 +12,22 @@ export const ArrowToggle = ({ orientation }: ArrowToggleProps) => {
   return (
     <div
       className={`arrow ${orientation} ${pressed ? 'pressed' : ''}`}
+      tabIndex={0}
       onMouseDown={() => setPressed(true)}
       onMouseUp={() => setPressed(false)}
       onMouseLeave={() => setPressed(false)}
+      // Accesibility options to keyboard users
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          setPressed(true)
+          e.preventDefault()
+        }
+      }}
+      onKeyUp={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          setPressed(false)
+        }
+      }}
     />
   )
 }
